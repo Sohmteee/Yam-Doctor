93f0482d2494527e866c77631b2403e1d9020e6d
Sohmteee <ukaegbesomtochukwu@gmail.com>
2024-08-24 07:14:26 +0100

    commit

--- lib/providers/chats.dart
+++ lib/providers/chats.dart
@@ -1,10 +1,11 @@
+import 'package:app/models/chatroom.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_chat_ui/flutter_chat_ui.dart';
 
 class ChatsProvider extends ChangeNotifier {
-  List<Chat> chats = [];
+  List<Chatroom> chats = [];
 
-  void addChat(Chat chat) {
+  void addChat(Chatroom chat) {
     chats.add(chat);
     notifyListeners();
   }
