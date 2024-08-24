da85bee2499b0d0aafa14a3476cc2130b71a8640
Sohmteee <ukaegbesomtochukwu@gmail.com>
2024-08-24 08:52:34 +0100

    commit

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -55,14 +55,30 @@ class _ChatScreenState extends State<ChatScreen> {
           dateHeaderBuilder: (p0) {
             String headerText;
             getDateDifference() {
-              final dayDiff = DateTime.now().difference(p0.dateTime).inDays;
-              if (dayDiff == 0) {
-                return 'Today';
-              } else if (dayDiff == 1) {
-                return 'Yesterday';
-              } else {
-                return '${p0.dateTime.month} ${p0.dateTime.day}${p0.dateTime.year == DateTime.now().year ? '' : ', ${p0.dateTime.year}'}';
-              }
+              // final dayDiff = DateTime.now().difference(p0.dateTime).inDays;
+              // if (dayDiff == 0) {
+              //   return 'Today';
+              // } else if (dayDiff == 1) {
+              //   return 'Yesterday';
+              // } else {
+              return '${
+                switch(p0.dateTime.month) {
+                  1=> 'January',
+                  2=> 'February',
+                  3=> 'March',
+                  4=> 'April',
+                  5=> 'May',
+                  6=> 'June',
+                  7=> 'July',
+                  8=> 'August',
+                  9=> 'September',
+                  10=> 'October',
+                  11=> 'November',
+                  12=> 'December',
+                  
+                }
+              } ${p0.dateTime.day}${p0.dateTime.year == DateTime.now().year ? '' : ', ${p0.dateTime.year}'}';
+              // }
             }
 
             headerText = getDateDifference();
