63a54112ad35296226101d32395e2c6a84a6c5c5
Sohmteee <ukaegbesomtochukwu@gmail.com>
2024-08-24 07:43:51 +0100

    commit

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -43,7 +43,7 @@ class _HomeScreenState extends State<HomeScreen> {
                 messages: messages,
                 chat: Chat(
                   onSendPressed: (types.Message message) {
-                    messages.add(
+                    messages.insert(0,
                       message
                     );
                   },
