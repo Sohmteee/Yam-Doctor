c8921ff408275b90c363e7dee129ae573427815f
Sohmteee <ukaegbesomtochukwu@gmail.com>
2024-08-24 07:06:46 +0100

    commit

---
+++ lib/providers/chats.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class ChatsProvider extends ChangeNotifier {
+  List<Chat> chats = [];
+
+
+  void addChat(Chat chat) {
+    chats.add(chat);
+    notifyListeners();
+  }
+
+  void removeChat(Chat chat) {
+    chats.remove(chat);
+    notifyListeners();
+  }
+}
