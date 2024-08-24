351586f4acfc0715723f042b265c7f07a901b995
Sohmteee <ukaegbesomtochukwu@gmail.com>
2024-08-24 08:05:35 +0100

    commit

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -1,5 +1,6 @@
 import 'package:app/models/chatroom.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_chat_ui/flutter_chat_ui.dart';
 
 class ChatScreen extends StatefulWidget {
   const ChatScreen({
@@ -16,6 +17,19 @@ class ChatScreen extends StatefulWidget {
 class _ChatScreenState extends State<ChatScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.chat.title),
+      ),
+      body: Chat(
+        messages: widget.chat.messages,
+        onSendPressed: (message) {
+          setState(() {
+            // widget.chat.messages.add(message);
+          });
+        },
+        user: widget.chat.chat.user,
+      ),
+    );
   }
 }
