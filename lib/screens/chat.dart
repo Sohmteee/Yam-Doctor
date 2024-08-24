e4b000664740bd82b30e369a9cf395da1d2d03d1
Sohmteee <ukaegbesomtochukwu@gmail.com>
2024-08-24 08:25:49 +0100

    commit

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -6,11 +6,13 @@ import 'package:flutter_chat_types/flutter_chat_types.dart' as types;
 import 'package:flutter_chat_ui/flutter_chat_ui.dart';
 import 'package:http/http.dart' as http;
 import 'package:image_picker/image_picker.dart';
-import 'package:intl/date_symbol_data_local.dart';
+// import 'package:intl/date_symbol_data_local.dart';
 import 'package:mime/mime.dart';
 import 'package:open_filex/open_filex.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:uuid/uuid.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
 
 class ChatScreen extends StatefulWidget {
   const ChatScreen({
