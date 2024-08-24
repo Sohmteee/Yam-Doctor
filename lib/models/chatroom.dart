419c776381e470f9165322cbb4afe325fc234594
Sohmteee <ukaegbesomtochukwu@gmail.com>
2024-08-24 07:13:20 +0100

    commit

---
+++ lib/models/chatroom.dart
@@ -0,0 +1,15 @@
+import 'package:flutter_chat_ui/flutter_chat_ui.dart';
+
+class Chatroom {
+  final String id;
+  final String title;
+  final Chat chat;
+
+  Chatroom({
+    required this.id,
+    required this.title,
+    required this.chat,
+  });
+
+  
+}
