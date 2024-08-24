b2dd6f8c268d50ef11551d881e11abe51d7187ac
Sohmteee <ukaegbesomtochukwu@gmail.com>
2024-08-24 15:11:52 +0100

    commit

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -24,7 +24,7 @@ class _HomeScreenState extends State<HomeScreen> {
       return Scaffold(
         appBar: AppBar(
           title: AppBoldText(
-            'Chats',
+            'Chat History',
           ),
         ),
         floatingActionButton: Padding(
